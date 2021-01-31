2a99ed4d2f0f5d7080ca2d9c23f065b4505ce8a6
Denis Mitiliuc <denis.mitiliuc@gmail.com>
2021-01-31 10:32:21 +0100

    adding some ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button'
+import NavigateNext from '@material-ui/icons/NavigateNext'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button 
+          variant="contained" 
+          color="primary"
+          startIcon={<NavigateNext />}
         >
-          Learn React
-        </a>
+          Hello
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
